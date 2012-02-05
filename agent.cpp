d36f63d8bf0387637a56da149575e890d28d671a
Alexander Batischev <eual.jp@gmail.com>
2012-02-05 20:13:12 +0200

    Example agent that moves randomly

--- agent.cpp
+++ agent.cpp
@@ -2,6 +2,10 @@
 #include "world.h"
 
 void Agent::act(World *world) {
-    /* Example agent does nothing */
-    world->performAction(doNothing);
+    /* Example agent moves randomly sucking as much dirt as possible */
+    if(world->isCurrentPosDirty()) {
+        world->performAction(suck);
+    } else {
+        world->performAction(static_cast<actions> (rand() % 4));
+    };
 }
